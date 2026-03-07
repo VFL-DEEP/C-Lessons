79178362579382eabfb4354c930329be1ccbe49d
hasanhuseyinakin <hasanhuseyinakin222@gmail.com>
2026-03-08 00:53:17 +0300

    Readme file added and changes done in first lesson.

--- 1: Variables.c
+++ 1: Variables.c
@@ -4,7 +4,9 @@
 int main() {
     int sayi = 100; 
     char karakter = 'B';
+    float ondaliklisayi =  3.12 ;
     printf("karakter: %c\n", karakter);
     printf("Sayi: %d\n", sayi);
-
+    printf("ondaliklisayi: %f\n", ondaliklisayi);
+    return 0;
 }
