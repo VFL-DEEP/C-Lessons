987ff3adc1f5e3c885011b807e8076324627a23a
hasanhuseyinakin <hasanhuseyinakin222@gmail.com>
2026-03-13 23:44:57 +0300

    A few mini fixes.

--- operators.c
+++ operators.c
@@ -1,18 +1,12 @@
 // This is the fourth lesson that i prepared for you :) In this lesson, i am going to teach you how to use operators in C programming language.
 // Operators are used to perform operations on variables and values. They are very important in programming because they allow us to calculate
-// numbers and compare values in our programs.
-//
-// There are different types of operators in C:
-//
+// numbers and compare values in our programs.  There are different types of opertors in C:
 // Arithmetic operators are used for mathematical operations like addition, subtraction, multiplication and division.
-// Examples: + (addition), - (subtraction), * (multiplication), / (division), % (modulus)
-//
-// Comparison operators are used to compare two values. They return either true (1) or false (0).
+// Examples: + (addition), - (subtraction), * (multiplication), / (division), % (modlus)
+// Comparrrisonn operators are used to compare two values. They return either true (1) or false (0).
 // Examples: == (equal), != (not equal), > (greater than), < (less than), >= (greater than or equal), <= (less than or equal)
-//
 // Logical operators are used to combine multiple conditions.
 // Examples: && (AND), || (OR), ! (NOT)
-//
 // So, to do these things, we first add the stdio.h library so we can use printf function.
 // After that, we create our main function and use different operators in example calculations.
 // Then we end our program with error code 0 which means the program executed successfully.
